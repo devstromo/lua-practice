70fc1911ebd2f6016ce357aff8501f0bdf287896
alemoles <alemolerio@gmail.com>
2025-02-21 21:58:57 -0300

    User single quotes and concatenation

--- 01-data-types.lua
+++ 01-data-types.lua
@@ -5,3 +5,11 @@ print(true)
 --  Global variables are declared starting with capital letters
 True = "Name"
 print(True)
+
+-- Single quotes
+First_Name = 'John'
+print(First_Name)
+local age = 25
+
+-- Concatenation
+print(First_Name .. " is " .. age .. " years old.")

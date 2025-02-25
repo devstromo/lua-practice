cf5fd819c1d51ae4e9330e1864664afcdb36053c
alemoles <alemolerio@gmail.com>
2025-02-25 18:45:04 -0300

    Maths operations

---
+++ 05-maths.lua
@@ -0,0 +1,40 @@
+-- 1. Addition
+local result = 4 + 3
+print(result)
+
+-- 2. Subtraction
+local result = 4 - 3
+print(result)
+
+-- 3. Multiplication
+local result = 4 * 3
+print(result)
+
+-- 4. Division
+local result = 4 / 3
+print(result)
+
+-- 5. Modulus
+local result = 4 % 3
+print(result)
+
+-- 6. Exponentiation
+local result = 4 ^ 3
+print(result)
+
+-- 7. Increment
+local x = 4
+x = x + 1
+print(x)
+
+-- 8. Decrement
+local x = 4
+x = x - 1
+print(x)
+
+-- 9. Comparison
+local result = 4 == 3
+print(result)
+
+local result = 4 ~= 3
+print(result)

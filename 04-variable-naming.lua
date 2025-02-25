aab1c3352a499c0b8f82a663ca3208d6830dd64e
alemoles <alemolerio@gmail.com>
2025-02-25 18:41:48 -0300

    Naming convetions

---
+++ 04-variable-naming.lua
@@ -0,0 +1,8 @@
+
+-- Variable names are case-sensitive
+local name = "John Doe"
+-- Variable names can contain letters, numbers, and underscores
+local age = 30
+local occupation = "Software Developer"
+-- Variable names cannot start with a number
+-- local 2s = "John Doe"

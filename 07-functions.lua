4524e607a724ea8739cbfc49ee16c505f86446b3
alemoles <alemolerio@gmail.com>
2025-03-04 18:10:03 -0300

    local and global scope

--- 07-functions.lua
+++ 07-functions.lua
@@ -1,4 +1,11 @@
 do
     local first_name = "John"
     print(first_name)
+    -- Global variables are accessible within the block.
+    FullName = first_name .. " Doe"
+    print(FullName)
 end
+print(first_name) -- nil
+-- The variable first_name is local to the block, so it is not accessible outside the block.
+print(FullName) -- John Doe
+-- The variable FullName is global, so it is accessible outside the block.

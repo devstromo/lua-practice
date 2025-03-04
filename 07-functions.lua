0d1273b8641472c7205daa13ca71e2b9cde0ed95
alemoles <alemolerio@gmail.com>
2025-03-04 18:08:04 -0300

    Functions

--- 07-functions.lua
+++ 07-functions.lua
@@ -1,9 +1,4 @@
-local first_name = "John"
-local last_name = "Doe"
-local full_name = first_name .. " " .. last_name
-print(full_name)
-print(#full_name) -- length of the string
-
-local start_char, end_char = string.find(full_name, "Doe")
-local restul = [[The string was found between the characters at position: ]] .. start_char .. [[ and ]] .. end_char .. [[.]]
-print(restul)
+do
+    local first_name = "John"
+    print(first_name)
+end

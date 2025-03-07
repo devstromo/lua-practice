7394e59559190c5642547cf5ea4c19019d75f31b
alemoles <alemolerio@gmail.com>
2025-03-06 21:01:24 -0300

    Loops

---
+++ 08-loops.lua
@@ -0,0 +1,17 @@
+function Print(...)
+    local args = { ... }
+    for i, v in args do
+        io.write(v)
+        if i < #args then
+            io.write("\t")
+        end
+    end
+    io.write("\n")
+end
+
+-- for loop
+for i = 1, 10 do
+    Print(i)
+end
+
+Print("John", "Doe", "Sally", "Smith", "Jane", "Doe")

12db40c8a6df4bd8690a63fb712f1e264c6fa06a
alemoles <alemolerio@gmail.com>
2025-03-06 21:18:29 -0300

    Using elseif statement

--- 09-control-flow.lua
+++ 09-control-flow.lua
@@ -9,3 +9,12 @@ if 1 > 2 then
 else
     print("1 is not greater than 2")
 end
+
+-- if elseif else statement
+if 1 > 2 then
+    print("1 is greater than 2")
+elseif 1 < 2 then
+    print("1 is less than 2")
+else
+    print("1 is not greater than 2 and 1 is not less than 2")
+end

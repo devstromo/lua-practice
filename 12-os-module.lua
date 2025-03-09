4f0a4062fc2c2fd1bc755384b91e00a4b93caaa5
alemoles <alemolerio@gmail.com>
2025-03-09 14:12:50 -0300

    Using the time from os module for time difference

--- 12-os-module.lua
+++ 12-os-module.lua
@@ -7,6 +7,10 @@ else
     print("Error removing file: " .. err)
 end
 
+function GetOS()
+    return package.config:sub(1, 1) == "\\" and "win" or "unix"
+end
+
 -- os.execute("ls -l")
 -- os.execute("rm files/*.a")
 
@@ -14,5 +18,18 @@ end
 -- print("First name: " .. first)
 
 -- Time
-local time = os.time()
-print("Time: " .. time)
+local start_time = os.time()
+print("Time: " .. start_time)
+
+if GetOS() == "win" then
+    -- Sleep once for 5 seconds instead of doing it inside the loop
+    os.execute("timeout /T 5 /NOBREAK > NUL")
+else
+    os.execute("sleep 5")
+end
+
+
+local end_time = os.time()
+print("Time: " .. end_time)
+
+print("Time difference: " .. os.difftime(end_time, start_time))

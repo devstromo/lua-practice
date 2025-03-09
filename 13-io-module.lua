ef3b743019e8b6a6dd3d0acaf219519995109484
alemoles <alemolerio@gmail.com>
2025-03-09 13:56:38 -0300

    Using the io module to append new info to a file

--- 13-io-module.lua
+++ 13-io-module.lua
@@ -1,4 +1,8 @@
-io.output("files/output.txt")
-io.write("Hello World from Lua\n")
-io.write("Hello World from second line\n")
-io.close()
+-- io.output("files/output.txt")
+-- io.write("Hello World from Lua\n")
+-- io.write("Hello World from second line\n")
+-- io.close()
+
+local file = io.open("files/output.txt", "a")
+file:write("Adding more text\n")
+file:close()

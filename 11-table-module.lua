99dfbc99af7249ad8178b8e123993c220df20e27
alemoles <alemolerio@gmail.com>
2025-03-09 13:23:40 -0300

    Using the table sort

--- 11-table-module.lua
+++ 11-table-module.lua
@@ -5,3 +5,15 @@ local t_names = {
     "Bob Johnson",
     "Carol Williams"
 }
+
+print("\nBefore sorting")
+for _, item in pairs(t_names) do
+    print(item)
+end
+
+table.sort(t_names)
+
+print("\nAfter sorting")
+for _, item in pairs(t_names) do
+    print(item)
+end

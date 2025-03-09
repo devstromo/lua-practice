79acf9d57f36c4e12f2a06d21d9db60def48afcf
alemoles <alemolerio@gmail.com>
2025-03-09 13:16:51 -0300

    Input data using io

--- 10-input.lua
+++ 10-input.lua
@@ -1,3 +1,8 @@
 io.write("What's your name?")
 local first_name = io.read()
 print("Hello, " .. first_name .. "!")
+
+io.write("How old are you?")
+local age = io.read()
+print("You are " .. age .. " years old." .. " Next year you will be " .. age + 1 .. " years old.")
+  

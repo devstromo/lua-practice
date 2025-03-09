842093e8a9c17a94681f7ab56092e843a8ea8c39
alemoles <alemolerio@gmail.com>
2025-03-09 13:17:59 -0300

    Input data using io

--- 10-input.lua
+++ 10-input.lua
@@ -3,6 +3,5 @@ local first_name = io.read()
 print("Hello, " .. first_name .. "!")
 
 io.write("How old are you?")
-local age = io.read()
+local age = tonumber(io.read()) -- tonumber converts a string to a numbers
 print("You are " .. age .. " years old." .. " Next year you will be " .. age + 1 .. " years old.")
-  
